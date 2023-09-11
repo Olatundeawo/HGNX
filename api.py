172dcd33d31843f6e0f53daf7eac5fddeaf00f06
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-09-10 19:35:03 -0700

    added the api to the route

--- api.py
+++ api.py
@@ -2,14 +2,16 @@
 """
 flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, request, Response
 from datetime import datetime
+import json
+from collections import OrderedDict
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def user():
     """ getting some query """
     slack_name = request.args.get('slack_name')
@@ -17,18 +19,21 @@ def user():
     dt = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     current_day = datetime.now().strftime('%A')
     status_code = 200
-
-    response_data = {
-        'slack_name': slack_name,
-        'current_day': current_day,
-        'utc_time': dt,
-        'track': track,
-        'github_file_url': 'https://github.com/Olatundeawo/HGNX/blob/main/api.py',
-        'github_repo_url': 'https://github.com/Olatundeawo/HGNX',
-        'status_code': status_code
-    }
-
-    return jsonify(response_data)
+    
+    response_data = OrderedDict()
+    response_data['slack_name'] = slack_name
+    response_data['current_day'] = current_day
+    response_data['utc_time'] = dt
+    response_data['track'] = track
+    response_data['github_file_url'] = 'https://github.com/Olatundeawo/HGNX/blob/main/api.py'
+    response_data['github_repo_url'] = 'https://github.com/Olatundeawo/HGNX'
+    response_data['status_code'] = status_code
+    
+    
+    json_data = json.dumps(response_data)
+    
+    response = Response(json_data, content_type='application/json')
+    return response
 
 
 if __name__ == '__main__':
